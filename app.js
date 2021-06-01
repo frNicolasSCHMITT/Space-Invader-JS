9eefa13284fc9ccdb3bb2756bc12e564b793287e
frNicolasSCHMITT <n.schmitt@codeur.online>
2021-06-01 15:57:02 +0200

    tuto done

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ let alien2 = new Sprite ("./images/alien2.png", 400, 50);
 let alien3 = new Sprite ("./images/alien3.png", 700, 50);
 let alien4 = new Sprite ("./images/alien4.png", 1000, 50);
 let alien5 = new Sprite ("./images/alien5.png", 1300, 50);
+let alien6 = new Sprite ("./images/alien1.png", 250, 150);
+let alien7 = new Sprite ("./images/alien2.png", 550, 150);
+let alien8 = new Sprite ("./images/alien3.png", 850, 150);
+let alien9 = new Sprite ("./images/alien4.png", 1150, 150);
+let alien10 = new Sprite ("./images/alien5.png", 1450, 150);
+let alien11 = new Sprite ("./images/alien1.png", 100, 250);
+let alien12 = new Sprite ("./images/alien2.png", 400, 250);
+let alien13 = new Sprite ("./images/alien3.png", 700, 250);
+let alien14 = new Sprite ("./images/alien4.png", 1000, 250);
+let alien15 = new Sprite ("./images/alien5.png", 1300, 250);
 
 function Sprite( filename, left, top ) {  //fonction de construction d'objet donc nom avec majuscule au début
     this._node = document.createElement("img");  //crée un élément de type img
@@ -100,9 +110,86 @@ document.onkeydown = function(event){  //track de la touche pressée
     }
 
     if(event.keyCode == 32){            // touche SPACEBAR = Tir missile
-        missile.display = "block";
-        missile.top = vaisseau.top; //positionne le missile sous le vaisseau
-        missile.left = vaisseau.left + (vaisseau._node.width - missile._node.width) / 2; //au centre du V
+        if (missile.display == "none"){  //lance un missile que si aucun affiché
+            missile.display = "block";  //affiche le missile
+            missile.top = vaisseau.top; //positionne le missile sous le vaisseau
+            missile.left = vaisseau.left + (vaisseau._node.width - missile._node.width) / 2; //au centre du V
+            missile.startAnimation(moveMissile, 20); //lance la fonction déplacement du missile toute les 20ms
+        }
     }
 
 };
+
+Sprite.prototype.startAnimation = function(fct, interval){
+    if (this._clock) window.clearInterval(this._clock);  //si dja une animation, la clear et remplace par la nouvelle
+    let _this = this;  //Pour récupérer le this précédent (Sprite)
+    this._clock = window.setInterval(function(){  //déclenche un délai régulier (this._clock)
+        fct(_this);  //fonction avec paramètre _this cité plus haut
+    },interval);
+}
+
+Sprite.prototype.stopAnimation = function(){ //En cas de stop d'animation
+    window.clearInterval(this._clock);  //clear le délai
+}
+
+function moveMissile(missile){
+    missile.top -=10;  //déplacement du missile en px / refresh fonction startAnimation(moveMissile)
+    if(missile.top < -40) {
+        missile.stopAnimation();
+        missile.display = "none";
+    }  //clear les missiles off-screen
+
+    for(var i=1; i<=15; i++){
+        var alien = window["alien"+i];
+        if (alien.display == "none") continue;
+        if (missile.checkCollision(alien)){
+            missile.stopAnimation();
+            missile.display = "none";
+            alien.stopAnimation();
+            alien.display = "none";
+        }
+    }
+}
+
+function moveAlienToRight(alien){
+    alien.left += 10;   //déplace les Aliens de 10 px vers la droite / refresh
+    if(alien.left > document.body.clientWidth - alien._node.width){
+        alien.top += 50;        //descend d'un étage
+        alien.startAnimation(moveAlienToLeft, 100);     //Puis va dans l'autre sens
+    }
+}
+
+function moveAlienToLeft(alien){
+    alien.left -= 10;   //déplace les Aliens de 10 px vers la gauche / refresh
+    if(alien.left <= 0){
+        alien.top += 50;    //descend d'un étage
+        alien.startAnimation(moveAlienToRight, 100);    //Puis va dans l'autre sens
+    }
+}
+
+// for(let i=1; i<=15; i++){       //variable où i= n° Alien
+//     window["alien"+i].startAnimation(moveAlienToRight, 100);
+// } marche pas??
+
+alien1.startAnimation(moveAlienToRight, 100);
+alien2.startAnimation(moveAlienToRight, 100);
+alien3.startAnimation(moveAlienToRight, 100);
+alien4.startAnimation(moveAlienToRight, 100);
+alien5.startAnimation(moveAlienToRight, 100);
+alien6.startAnimation(moveAlienToRight, 100);
+alien7.startAnimation(moveAlienToRight, 100);
+alien8.startAnimation(moveAlienToRight, 100);
+alien9.startAnimation(moveAlienToRight, 100);
+alien10.startAnimation(moveAlienToRight, 100);
+alien11.startAnimation(moveAlienToRight, 100);
+alien12.startAnimation(moveAlienToRight, 100);
+alien13.startAnimation(moveAlienToRight, 100);
+alien14.startAnimation(moveAlienToRight, 100);
+alien15.startAnimation(moveAlienToRight, 100);
+
+Sprite.prototype.checkCollision = function (other){     //collision si n'est pas
+    return ! ((this.top + this._node.height < other.top) ||     // au dessus ou
+            this.top > (other.top + other._node.height) ||      // en dessous ou
+            (this.left + this._node.width < other.left) ||      // avant ou
+            this.left > (other.left + other._node.width));      // après
+}
